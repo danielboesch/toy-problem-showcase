29005fdd0e212e47f26f503dfdcc3e27bb5e9c6f
Daniel Boesch <you@example.com>
2021-04-20 19:52:27 -0600

    done

---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class FilterString extends Component{
+    render(){
+        return(
+            <p>Filter String</p>
+        )
+    }
+}
+export default FilterString
